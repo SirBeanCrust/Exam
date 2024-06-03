c0f3f0e90b39045feca53e2151ca9f0d9f28f53b
Mathias <matmag14@innlandetfylke.no>
2024-06-03 14:22:57 +0200

    Object.map for testing and display of info + add to MyLoans

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./Css/App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const objectArray = [
+  {
+    produsent: "Hewlett Packard",
+    beskrivelse: "HP Envy Desktop TE01-4254",
+    spesifikasjoner:
+      "Intel Core i7-13700, 16GB RAM, 1TB SSD, Intel UHD Graphics 770, Windows 11 Home",
+    innkjøpsdato: "15.08.2023",
+    innkjøpspris: 999.99,
+    "forventet levetid (i år)": 5,
+    kategori: "Datamaskiner",
+  },
+  {
+    produsent: "Dell",
+    beskrivelse: "Dell XPS 15",
+    spesifikasjoner:
+      "Intel Core i7-12700H, 16GB RAM, 512GB SSD, NVIDIA GeForce RTX 3050 Ti, Windows 11 Pro",
+    innkjøpsdato: "12.06.2023",
+    innkjøpspris: 1500.0,
+    "forventet levetid (i år)": 4,
+    kategori: "Bærbare Datamaskiner",
+  },
+];
+
+function Items({ setSelectedItem }) {
+  return (
+    <div>
+      {objectArray.map((item, i) => (
+        <div key={i}>
+          <button onClick={() => setSelectedItem(item)}>
+            <p>{item.beskrivelse}</p>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Display({ selectedItem, setMyLoans, myLoans }) {
+  function leggTil() {
+    if (selectedItem) {
+      setMyLoans((prevMyLoans) => [...prevMyLoans, selectedItem]);
+      console.log({myLoans})
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>Produsent: {selectedItem?.produsent}</p>
+      <p>Beskrivelse: {selectedItem?.beskrivelse}</p>
+      <p>Spesifikasjoner: {selectedItem?.spesifikasjoner}</p>
+      <p>Innkjøpsdato: {selectedItem?.innkjøpsdato}</p>
+      <p>Innkjøpspris: {selectedItem?.innkjøpspris}</p>
+      <p>
+        Forventet levetid: (i år):{" "}
+        {selectedItem?.["forventet levetid (i år)"]}
       </p>
+      <p>Kategori:{selectedItem?.kategori}</p>
+
+      <button onClick={leggTil}>Lån</button>
     </>
-  )
+  );
 }
 
-export default App
+function MyLoans(){
+  <>
+
+  </>
+}
+
+export default function App() {
+  const [selectedItem, setSelectedItem] = useState({});
+  const [myLoans, setMyLoans] = useState([]);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Items setSelectedItem={setSelectedItem} />
+      <Display selectedItem={selectedItem} setMyLoans={setMyLoans} myLoans={myLoans} />
+      <MyLoans/>
+    </>
+  );
+}
